f3035ceb4c67e84741b09b4391bbcdda4e088bc0
Jacob Zahn <jmzahn@satx.rr.com>
2020-04-26 06:29:59 -0500

    fix test cases

--- find.c
+++ find.c
@@ -8,13 +8,15 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 
-void find_recursive(char *where_to_look,const char *args[3]);
+void find_recursive(char *where_to_look, const char *args[3]);
+
+void checkCriteria(char *where_to_look, const char *args[3], char* temp_full_path, char* temp, struct stat f_stat);
 
 int main(int argc, char *argv[])
 {
     int pathGiven = 1;
     char *whereToLook;
-    char buf[PATH_MAX];
+    char buf[_PC_PATH_MAX];
     const char *args[3];
     args[0] = "";
     args[1] = "";
@@ -101,21 +103,26 @@ void find_recursive(char *where_to_look, const char *args[3])
     //check whether the directory stream is opened successfully
     if(sub_dp!=NULL)
     {
-        if(strcmp(args[0],"")==0){
-            printf("%s\n",where_to_look);
+        //check criteria on current dir only if its a dir
+        char *tempD ="";
+        if(stat(where_to_look,&f_stat)==0){
+            if((f_stat.st_mode & __S_IFMT) == __S_IFDIR)
+                checkCriteria(where_to_look, args, where_to_look, tempD, f_stat);
         }
-        
+        else{
+            perror("stat error");
+        }
+        //then look at files/dirs in dir
         //read one entry at a time, and stop when none left
         while((sub_dirp=readdir(sub_dp))!=NULL)
         {
             //check whether the first entry is a subdirectory
             char * temp =sub_dirp->d_name;
-            //printf("temp: %s\n",temp);
             //to avoid recursively searching . and .. in the directory.
-            
             if(strcmp(temp,temp1)!=0&&strcmp(temp,temp2)!=0)
             {
-                char* temp_full_path=malloc(sizeof(char)*2000);
+                //printf("temp: %s\n",temp);
+                char *temp_full_path=malloc(sizeof(char)*2000);
                 char *temp_sub=malloc(sizeof(char)*100);
                 temp_sub=strcpy(temp_sub,temp3);
                 temp_sub=strcat(temp_sub,temp);
@@ -125,6 +132,16 @@ void find_recursive(char *where_to_look, const char *args[3])
                 temp_full_path = strcat(temp_full_path,temp_sub);
                 //now you get a full path, e.g., testdir/dir1 or testdir/test1
                 //printf("temp_full_path: %s\n",temp_full_path);
+                
+                //check criteria only if looking at regular file
+                if(stat(temp_full_path,&f_stat)==0){
+                    if((f_stat.st_mode & __S_IFMT) == __S_IFREG)
+                        checkCriteria(where_to_look, args, temp_full_path, temp, f_stat);
+                }
+                else{
+                    perror("stat error");
+                }
+                
                 // try to open
                 DIR * subsubdp=opendir(temp_full_path);
                 //if not null, means we find a subdirectory, otherwise, its just a file
@@ -134,137 +151,8 @@ void find_recursive(char *where_to_look, const char *args[3])
                     closedir(subsubdp);
                     find_recursive(temp_full_path,args);//call the recursive function call.
                 }
-                else
-                {
-                    closedir(subsubdp);
-                    //check if file
-                    //printf("stat return: %d\n",stat(temp_full_path,&f_stat));
-                    if(stat(temp_full_path,&f_stat)==0)
-                    {
-                        //if criteria empty then print or delete
-                        //printf("args[0] \"%s\"\n", args[0]);
-                        //printf("args[1] \"%s\"\n", args[1]);
-                        //printf("args[2] \"%s\"\n", args[2]);
-                        //printf("strcmp return:  %d\n", strcmp(args[0],""));
-                        
-                        if(strcmp(args[0],"")==0)
-                        {
-                            //printf("IN if(strcmp(args[0],\"\"))");
-                            //if -delete
-                            if(strcmp(args[2],"-delete")==0)
-                            {
-                                remove(sub_dirp->d_name);
-                            }
-                            else
-                            {
-                                //printf("IN else of Del");
-                                //print the filename
-                                printf("%s\n",temp_full_path);
-                            }
-                        }
-                        //else check criteria
-                        else
-                        {
-                            if(strcmp(args[0],"-name")==0)
-                            {
-                                //check if name match
-                                //printf("strcmp return:  %d\n", strcmp(args[1],temp));
-                                if(strcmp(args[1],temp)==0)
-                                {
-                                    
-                                    //if -delete
-                                    if(strcmp(args[2],"-delete")==0)
-                                    {
-                                        remove(sub_dirp->d_name);
-                                    }
-                                    else
-                                    {
-                                        //print the first entry, a file or a subdirectory
-                                        printf("%s\n",temp_full_path);
-                                    }
-                                }
-                            }
-                            else if(strcmp(args[0],"-mmin")==0)
-                            {
-                                //get num and sign
-                                char *num = malloc(sizeof(char)*200);
-                                num = strcpy(num,args[1]);
-                                char sign = num[0];
-                                num=num+1;
-                                
-                                //get last modification time
-                                time_t lastModTime = f_stat.st_mtime;
-                                //get now
-                                time_t rawtime;
-                                time(&rawtime);
-                                //get difference in secs
-                                double diffsecs = difftime(lastModTime,rawtime);
-                                int diffmins = diffsecs/60;
-                                //check sign of time
-                                if(strcmp(&sign,"-")==0)
-                                {
-                                    //if mins since modified less than num provided
-                                    if(diffmins<atoi(num))
-                                    {
-                                        //if -delete
-                                        if(strcmp(args[2],"-delete")==0)
-                                        {
-                                            unlink(temp_full_path);
-                                        }
-                                        else
-                                        {
-                                            //print the first entry, a file or a subdirectory
-                                            printf("%s\n",temp_full_path);
-                                        }
-                                    }
-                                    
-                                }
-                                else if(strcmp(&sign,"+")==0)
-                                {
-                                    //if mins since modified greater than num provided
-                                    if(diffmins>atoi(num))
-                                    {
-                                        //if -delete
-                                        if(strcmp(args[2],"-delete")==0)
-                                        {
-                                            unlink(temp_full_path);
-                                        }
-                                        else
-                                        {
-                                            //print the first entry, a file or a subdirectory
-                                            printf("%s\n",temp_full_path);
-                                        }
-                                    }
-                                }
-                                free(num);
-                            }
-                            else if(strcmp(args[0],"-inum")==0)
-                            {
-                                //printf("strcmp return:  %d\n", strcmp(args[0],"-inum"));
-                                char *tempstr=malloc(sizeof(char)*200);
-                                sprintf(tempstr,"%lu", f_stat.st_ino);
-                                
-                                //printf("strcmp return:  %d\n", strcmp(args[1],tempstr)==0);
-                                if(strcmp(args[1],tempstr)==0)
-                                {
-                                    //if -delete
-                                    if(strcmp(args[2],"-delete")==0)
-                                    {
-                                        unlink(temp_full_path);
-                                    }
-                                    else
-                                    {
-                                        //print the first entry, a file or a subdirectory
-                                        printf("%s\n",temp_full_path);
-                                    }
-                                }
-                                free(tempstr);
-                            }
-                        }
-                    }
-                }
-               free(temp_sub);
-               free(temp_full_path);
+                free(temp_sub);
+                free(temp_full_path);
             }
             
         }
@@ -275,4 +163,141 @@ void find_recursive(char *where_to_look, const char *args[3])
         exit(2);
     }
     
+}
+
+void checkCriteria(char *where_to_look, const char *args[3], char* temp_full_path, char* temp, struct stat f_stat){
+    if(strcmp(args[0],"")==0)
+    {
+        //if -delete
+        if(strcmp(args[2],"-delete")==0)
+        {
+            if(remove(temp_full_path)!=0){
+                printf("find: cannot delete %s",temp_full_path);
+            }
+        }
+        else
+        {
+            //print the filename
+            printf("%s\n",temp_full_path);
+        }
+    }
+    //else check criteria
+    else
+    {
+        if(strcmp(args[0],"-name")==0)
+        {
+            //check if name match
+            if(strcmp(args[1],temp)==0)
+            {
+                
+                //if -delete
+                if(strcmp(args[2],"-delete")==0)
+                {
+                    if(remove(temp_full_path)!=0){
+                        printf("find: cannot delete %s",temp_full_path);
+                    }
+                }
+                else
+                {
+                    //print the first entry, a file or a subdirectory
+                    printf("%s\n",temp_full_path);
+                }
+            }
+        }
+        else if(strcmp(args[0],"-mmin")==0)
+        {
+            //printf("-mmin\n");
+            //get num and sign
+            char *num = malloc(sizeof(char)*200);
+            num = strcpy(num,args[1]);
+            char sign;
+            if(num[0]=='-'){
+                sign = '-';
+                memmove(num, num+1, strlen(num));
+            }
+            else if(num[0]=='+'){
+                sign = '+';
+                memmove(num, num+1, strlen(num));
+            }
+            
+            //get last modification time
+            time_t lastModTime = f_stat.st_mtime;
+            //get now
+            time_t rawtime;
+            time(&rawtime);
+            //get difference in secs
+            double diffsecs = difftime(lastModTime,rawtime);
+            int diffmins = diffsecs/60;
+            //check sign of time
+            
+            //printf("Sign value: %d\n",sign);
+            //printf("Num value: %s\n",num);
+            //printf("Last mod time: %ld\n",lastModTime);
+            //printf("diffmins: %d\n",diffmins);
+            if(sign=='-')
+            {
+                //if mins since modified less than num provided
+                if(diffmins>atoi(num))
+                {
+                    //if -delete
+                    if(strcmp(args[2],"-delete")==0)
+                    {
+                        if(remove(temp_full_path)!=0){
+                            printf("find: cannot delete %s",temp_full_path);
+                        }
+                    }
+                    else
+                    {
+                        //print the first entry, a file or a subdirectory
+                        printf("%s\n",temp_full_path);
+                    }
+                }
+                
+            }
+            else if(sign=='+')
+            {
+                //if mins since modified greater than num provided
+                if(diffmins<atoi(num))
+                {
+                    //if -delete
+                    if(strcmp(args[2],"-delete")==0)
+                    {
+                        if(remove(temp_full_path)!=0){
+                            printf("find: cannot delete %s",temp_full_path);
+                        }
+                    }
+                    else
+                    {
+                        //print the first entry, a file or a subdirectory
+                        printf("%s\n",temp_full_path);
+                    }
+                }
+            }
+            free(num);
+        }
+        else if(strcmp(args[0],"-inum")==0)
+        {
+            //printf("strcmp return:  %d\n", strcmp(args[0],"-inum"));
+            char *tempstr=malloc(sizeof(char)*200);
+            sprintf(tempstr,"%lu", f_stat.st_ino);
+            
+            //printf("strcmp return:  %d\n", strcmp(args[1],tempstr)==0);
+            if(strcmp(args[1],tempstr)==0)
+            {
+                //if -delete
+                if(strcmp(args[2],"-delete")==0)
+                {
+                    if(remove(temp_full_path)!=0){
+                        printf("find: cannot delete %s",temp_full_path);
+                    }
+                }
+                else
+                {
+                    //print the first entry, a file or a subdirectory
+                    printf("%s\n",temp_full_path);
+                }
+            }
+            free(tempstr);
+        }
+    }
 }
