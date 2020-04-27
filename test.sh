2d4943bd58198a3758ed47bdb9077a301020915c
Jacob Zahn <jmzahn@satx.rr.com>
2020-04-27 01:50:17 -0500

    update test script

--- test.sh
+++ test.sh
@@ -4,29 +4,43 @@ echo -e "\nTest: find where-to-look\n"
 echo -e "Linux utility:\n"
 find testdir
 echo -e "\nMy find utility:\n"
-./find testdir
+./find -w testdir
 
 echo -e "\nTest: find where-to-look -name <name>\n"
 echo -e "Linux utility:\n"
 find testdir -name test5
 echo -e "\nMy find utility:\n"
-./find testdir -name test5
+./find -w testdir -n test5
 
 echo -e "\nTest: find where-to-look -mmin <num>\n"
 echo -e "Linux utility:\n"
 find testdir -mmin +5
 echo -e "\nMy find utility:\n"
-./find testdir -mmin +5
+./find -w testdir -m +5
 
 echo -e "\nTest: find where-to-look -inum <num>\n"
 echo -e "Linux utility:\n"
 find testdir -inum 1125899906951215
 echo -e "\nMy find utility:\n"
-./find testdir -inum 1125899906951215
+./find -w testdir -i 1125899906951215
 
 echo -e "\nTest: find where-to-look criteria -delete\n"
 echo -e "Linux utility:\n"
 find testdir -name test1 -delete
 echo -e "\nMy find utility:\n"
-./find testdir -name test2 -delete
+./find -w testdir -n test2 -d
 
+echo -e "\nTest: find where-to-look criteria -action\n"
+echo -e "Linux utility: find testdir -name test1 -exec cat {} \\;\n"
+find testdir -name test4 -exec cat {} \;
+echo -e "\nMy find utility:\n"
+./find -w testdir -n test4 -a cat
+
+echo -e "\nMy find utility: touch\n"
+./find -w testdir -n test4 -a touch touchtest
+
+echo -e "\nMy find utility: rm\n"
+./find -w testdir -n touchtest -a rm
+
+echo -e "\nMy find utility: mv\n"
+./find -w testdir -n test4 -a mv test44
